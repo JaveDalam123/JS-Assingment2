4094decb499894196b4ad6c1d0cdc93f5502536d
Javed Alam <javedalam96542@gmail.com>
2025-02-17 01:24:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ for(let r=1;r<=n;r++)
 let num=45;
  for(let i=1;i<=num;i++)
      {
-   if(i%3==0 && i%5!=0)
+   if(i%3==0)
     {
         console.log("Fizz");
     }
-    else if(i%3!==0&& i%5==0)
+    if(i%3==0)
     {
         console.log("Buzz");
     }
-    else if(i%3==0 && i%5==0)
+     if(i%3==0 && i%5==0)
     {
         console.log("FizzBuzz");
     }
